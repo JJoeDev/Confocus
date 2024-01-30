b4ae9f8dbbbc77837e15eefbd2b3ba206372d139
jjoedev <jhknuds1@protonmail.com>
2024-01-30 23:10:20 +0100

    Added functionallity for arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include <cctype>
+#include <cstring>
 #include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <string>
+
+#if defined(_WIN32) || defined(_WIN64)
+#define CLS "cls"
+#else
+#define CLS "clear"
+#endif
 
 /*
 *
@@ -10,8 +21,53 @@
 *
 */
 
+inline void consoleClear() { system(CLS); }
+
+void checkArgs(const char* args);
+void printHelp();
+bool checkIfNumber(const char* input);
+
+const char* welcomeText = "╦ ╦┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ╔═╗┌─┐┌┐┌┌─┐┌─┐┌─┐┬ ┬┌─┐\n"\
+                          "║║║├┤ │  │  │ ││││├┤    │ │ │  ║  │ ││││├┤ │ ││  │ │└─┐\n"\
+                          "╚╩╝└─┘┴─┘└─┘└─┘┴ ┴└─┘   ┴ └─┘  ╚═╝└─┘┘└┘└  └─┘└─┘└─┘└─┘";
+
+float timer{0};
+
 int main (int argc, char *argv[]) {
-  std::cout << "Welcome to Confocus!\n";
+  consoleClear();
+  std::cout << welcomeText << std::endl;
   
+  if(argc > 1)
+    checkArgs(argv[1]);
+
+  std::cout << timer << '\n';
+
   return 0;
+}
+
+bool checkIfNumber(const char* input){
+  if(input == nullptr || *input == '\0') return false;
+
+  for(; *input != '\0'; ++input){
+    if(!std::isdigit(*input)){
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void printHelp(){
+  std::cout << "Confocus can be started with no arguments and with arguments\n[ ARG ] -h | This prints this help page\n[ ARG ] [num] This starts a timer at the given time (30 = 30 sec)" << std::endl;
+}
+
+void checkArgs(const char* arg){
+  if(std::strcmp(arg, "-h") == 0){
+    printHelp();
+  }
+  else{
+    if(checkIfNumber(arg)){
+      timer = std::strtof(arg, nullptr);
+    }
+  }
 }
