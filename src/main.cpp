327d531b844db437f1bf4f30cb141baa169ee687
jjoedev <jhknuds1@protonmail.com>
2024-01-31 22:05:11 +0100

    parsing input to time

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
-#include <string>
+#include <sstream>
+#include <stdexcept>
 
 #if defined(_WIN32) || defined(_WIN64)
 #define CLS "cls"
@@ -23,9 +24,14 @@
 
 inline void consoleClear() { system(CLS); }
 
+// Arguments 
 void checkArgs(const char* args);
 void printHelp();
 bool checkIfNumber(const char* input);
+
+// Get Timer (in app)
+void SetTime();
+std::chrono::seconds durationParse(const std::string& input);
 
 const char* welcomeText = "╦ ╦┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ╔═╗┌─┐┌┐┌┌─┐┌─┐┌─┐┬ ┬┌─┐\n"\
                           "║║║├┤ │  │  │ ││││├┤    │ │ │  ║  │ ││││├┤ │ ││  │ │└─┐\n"\
@@ -39,10 +45,50 @@ int main (int argc, char *argv[]) {
   
   if(argc > 1)
     checkArgs(argv[1]);
+  else
+    SetTime();
 
   std::cout << timer << '\n';
 
   return 0;
+}
+
+std::chrono::seconds durationParse(const std::string& input){
+  std::istringstream iss(input);
+  int value;
+  char unit;
+
+  if(!(iss >> value >> unit)){
+    throw std::invalid_argument("Invalid input format. Allowed uses are s, h, m");
+  }
+  switch (unit) {
+  case 's':
+    return std::chrono::seconds(value);
+    break;
+  case 'm':
+    return std::chrono::minutes(value);
+    break;
+  case 'h':
+    return std::chrono::hours(value);
+    break;
+  default:
+    return std::chrono::seconds(30);
+    break;
+  }
+}
+
+void SetTime(){
+  consoleClear();
+  std::cout << "How long would you like to work focused? (10s = 10sec 1m = 1min)\n";
+  std::string input;
+  std::cin >> input;
+
+  try{
+    auto duration = durationParse(input);
+    std::cout << "Duration: " << duration.count() << std::endl;
+  } catch (const std::invalid_argument& e){
+    std::cerr << "[ ERROR ]: " << e.what() << std::endl;
+  }
 }
 
 bool checkIfNumber(const char* input){
